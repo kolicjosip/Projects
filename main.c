899451e3576b840642a0d24f899213aa28decf75
kolicjosip <141852102+kolicjosip@users.noreply.github.com>
2025-05-23 15:08:44 +0200

    SPA - DZ02/

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+typedef int labeltype;
+#include "BinaryTree.h"
+
 typedef int elementtype;
+#include "List.h"
 
-#include "Stack.h"
-#include "Prefix.h"
+#include "Huffman.h"
 
-int main(void) {
-    char izraz1[] = "<-> -> T & F T | F - T",
-     izraz2[] = "<-> -> T & F T | F T -";
-    printf("%d\n", PrefixEval(izraz1));
-    printf("%d", PrefixEval(izraz2));
+int main(void){
+    List L;
+    LiMakeNull(&L);
+
+    int brojevi[7] = {3,5,6,3,2,4,4};
+    int i;
+    for(i = 0; i < 7; ++i) {
+        LiInsert(brojevi[i], LiEnd(L), &L);
+    }
+
+    printf("Vrijeme izvrsavanja je %d.", PrintTree(HuffmanTree(L)));
+    return 0;
 }
